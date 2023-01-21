619c4248f46b30af89d587469511aeca273d29a0
Kutuwa <kutuwaruksh.55@gmail.com>
2023-01-21 14:55:48 +0545

    added staticfile directory in setting

--- e_store/urls.py
+++ e_store/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from auth_users import views as auth_views
 
 
 urlpatterns = [
@@ -27,7 +26,7 @@ urlpatterns = [
 
     # path('', include('django.contrib.auth.urls'))
     
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
