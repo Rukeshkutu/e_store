ea447e5ba72c99b560a9bf5200145deccd215133
Kutuwa <kutuwaruksh.55@gmail.com>
2023-01-26 14:01:00 +0545

    forgot password in signin page

--- e_store/urls.py
+++ e_store/urls.py
@@ -17,12 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls', namespace='store')),
     path('auth/', include('auth_users.urls')),
+    
+    path('password-reset-done/', auth.PasswordResetDoneView.as_view(template_name='auth_users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth.PasswordResetConfirmView.as_view(template_name="auth_users/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset-done/', auth.PasswordResetCompleteView.as_view(template_name='auth_users/password_reset_complete.html'), name='password_reset_complete'),     
+
     
 ]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
