d1b917d35c05a1d26f799e95f80a61fd2e019d0a
Kutuwa <kutuwaruksh.55@gmail.com>
2023-01-29 20:00:19 +0545

    user profile is added

--- auth_users/models.py
+++ auth_users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.png', upload_to='profile_image')
+    city = models.CharField(max_length=50, blank=True)
+    zone = models.CharField(max_length=50, blank=True)
+    contact_no = models.CharField(max_length=12, blank=True)
+    tole = models.CharField(max_length=50, blank=True)
+    bio = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse("auth_users:profile", args=[self.slug])
+
+    def __str__(self):
+        return self.user.username
--- auth_users/urls.py
+++ auth_users/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('register/', views.register_page, name = 'register'),
     
     path('password-reset/', views.password_reset_request, name ="password_reset"),
+    
+    path('profile/', views.profile, name= 'profile'),
+    path('edit-profile-image', views.edit_profile_image, name = "edit_profile_image"),
+    path('edit-profile-detail', views.edit_profile_detail, name = "edit_profile_detail"),
+
 
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name = 'admin_dashboard'),
